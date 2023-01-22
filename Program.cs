07f800a359684396771a2f78e34254d8bbadc4b3
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2023-01-22 19:41:00 -0300

    Finalização desafio abrstraindo smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia...");
+Nokia nokia = new Nokia(numero: "123456", modelo: "Tijolão", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smarphone Iphone...");
+Iphone iphone = new Iphone(numero: "654321", modelo: "14 Plus", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
